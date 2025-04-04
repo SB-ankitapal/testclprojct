5bd69807571e2d0131d2afd334ee558e23f43995
ankita.pal@smartbear.com <ankita.pal@smartbear.com>
2025-04-04 15:56:29 +0530

    test

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://x-19938.stoplight-dev.com/api/v1/projects/cHJqOjQyNg/spectral.js?branch_slug=main&token=254fb3c7-75ff-4316-b64f-85a5d132f64b";
+export default { extends: ruleset };
